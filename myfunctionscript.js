80d8db9e44a14e747fdf8b9d812be9aaa388b3e4
jaweber64 <jaWeber@janets-mbp.attlocal.net>
2016-09-04 13:18:40 -0500

    Initial commit

---
+++ myfunctionscript.js
@@ -0,0 +1,73 @@
+/**
+ * Created by jaWeber on 9/4/16.
+ */
+
+// Tried code below for text below large image when page loads, but didn't work.
+//d1text = "Daisy, pure bred Female Rhodesian Ridgeback:  2000 - 2013";
+//document.getElementById("largeimagetext").innerHTML = d1text;
+
+// These functions change the large image and assoicated text for 11 thumbnails
+function change01(){
+    document.getElementById("large").src="images/D1.png";
+    document.getElementById("largeimagetext").innerHTML = d1text;
+}
+
+function change02(){
+    s1text = "Our old lady, Star - Female Pekenese/Beagle Mix (rescue) - a.k.a. little terrorist!";
+    document.getElementById("large").src="images/S1.png";
+    document.getElementById("largeimagetext").innerHTML = s1text;
+}
+
+function change03(){
+    sd1text = "Star & Daisy - Chillin";
+    document.getElementById("large").src="images/SD1.png";
+    document.getElementById("largeimagetext").innerHTML = sd1text;
+}
+
+function change04(){
+    m1text = "Marley - Female Malinois/Shepherd Mix (rescue) - as a puppy";
+    document.getElementById("large").src="images/M1.png";
+    document.getElementById("largeimagetext").innerHTML = m1text;
+}
+
+function change05(){
+    l3text = "Lacey - Female Shepherd Mix (rescue) - as a puppy";
+    document.getElementById("large").src="images/L3.png";
+    document.getElementById("largeimagetext").innerHTML = l3text;
+}
+
+function change06(){
+    ml1text = "Marley & Lacey - rescued a week apart (different litters)";
+    document.getElementById("large").src="images/ML1.png";
+    document.getElementById("largeimagetext").innerHTML = ml1text;
+}
+
+function change07(){
+    s2text = "Star - cowgirl!  Fun at the family reunion!";
+    document.getElementById("large").src="images/S2.png";
+    document.getElementById("largeimagetext").innerHTML = s2text;
+}
+
+function change08(){
+    threeDogstext = "Star, Marly & Lacey enjoying the fire (and each other)!";
+    document.getElementById("large").src="images/threeDogs.png";
+    document.getElementById("largeimagetext").innerHTML = threeDogstext;
+}
+
+function change09(){
+    m2text = "Marley - causing trouble!";
+    document.getElementById("large").src="images/M2.png";
+    document.getElementById("largeimagetext").innerHTML = m2text;
+}
+
+function change10(){
+    l2text = "More Marley trouble!";
+    document.getElementById("large").src="images/L2.png";
+    document.getElementById("largeimagetext").innerHTML = l2text;
+}
+
+function change11(){
+    threeDogs2text = "We travel well!";
+    document.getElementById("large").src="images/threeDogs2.png";
+    document.getElementById("largeimagetext").innerHTML = threeDogs2text;
+}
